9722d68cbd9c1867d014c0c1b8ef9a7681011f60
mhjesus <mhjesus@indracompany.com>
2019-06-17 14:49:41 -0300

    Ajustes revisão 3

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -1,76 +1,78 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Books from './Books'
-
+import React from "react";
+import {Link} from "react-router-dom";
+import Books from "./Books";
 
 class SearchBook extends React.Component {
+  state = {
+    query: "",
+    searchBooks: []
+  };
 
-    state = {
-        query: '',
-        searchBooks: []
-    }
+  updateQuery = query => {
+    this.setState({
+      query: query
+    });
+    this.searchBook(this.state.query);
+  };
 
-    updateQuery = (query) => {
-        this.setState({ 
-            query: query,
-        });
-        this.searchBook(this.state.query)
-    }
+  searchBook(query) {
 
-    searchBook(query) {
-        this.props.onSearchBook(query).then((bookResults) => {
-            this.setState({
-                searchBooks: bookResults ? bookResults : []
-            })
-        })
-        
-    }
+    if (this.state.query && this.props.books) {
+      this.props.onSearchBook(query).then(bookResults => {
+        console.log("bookResults", bookResults);
 
-    clearQuery = () => {
-        this.setState({ query: '' });
-    }
+        if (bookResults && bookResults.length > 0) {
 
-    render() {
-        
-        const { query, searchBooks } = this.state;
-        const { onUpdateBook } = this.props;
-        let showingBooks;
-
-        if (query) {
-            showingBooks = searchBooks
+            for (var el in bookResults) {
+                let bR = bookResults[el]
+                let book = this.props.books.find(b=> b.id === bR.id)
+                if(book) {
+                    bR.shelf = book.shelf
+                }
+            }
+          this.setState({
+            searchBooks: bookResults
+          });
         } else {
-            showingBooks = []
+          this.setState({
+            searchBooks: []
+          });
         }
-
-        return(
-            <div className="search-books">
-                <div className="search-books-bar">
-                    <Link to="/">
-                        <button className="close-search">Close</button>
-                    </Link>
-                    <div className="search-books-input-wrapper">
-                        <input 
-                            type="text" 
-                            placeholder="Search by title or author"
-                            value={query}
-                            onChange={(event) => this.updateQuery(event.target.value)}
-                        />
-                    </div>
-                </div>
-                <div className="search-books-results">
-                    { query ? 
-                        (showingBooks.length > 0 ?
-                            <Books 
-                                title="" 
-                                books={showingBooks}
-                                onUpdateBook={onUpdateBook}
-                            />
-                        : 'livro nao encontrado') : ''
-                    }
-                </div>
-            </div>
-        )
+      });
+    } else {
+        this.setState({searchBooks: []})
     }
+  }
+
+  clearQuery = () => {
+    this.setState({query: ""});
+  };
+
+  render() {
+    const {query, searchBooks} = this.state;
+    const {onUpdateBook} = this.props;
+    let showingBooks;
+
+    if (query) {
+      showingBooks = searchBooks;
+    } else {
+      showingBooks = [];
+    }
+
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to="/">
+            <button className="close-search">Close</button>
+          </Link>
+          <div className="search-books-input-wrapper">
+            <input type="text" placeholder="Search by title or author" value={query} onChange={event => this.updateQuery(event.target.value)} />
+          </div>
+        </div>
+        <div className="search-books-results">{query ? showingBooks.length > 0 ? <Books title="" books={showingBooks} onUpdateBook={onUpdateBook} /> : "livro nao encontrado" : ""}</div>
+      </div>
+    );
+  }
 }
 
-export default SearchBook
+export default SearchBook;
